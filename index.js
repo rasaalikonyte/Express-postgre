f4e2ffe6754ee91cc7d9cae063dea76bc0065693
Your Name <rasa.alikonyte@gmail.com>
2024-05-13 13:58:33 +0300

    updated pg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const pool = require("./db");
+const pool = require("db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
