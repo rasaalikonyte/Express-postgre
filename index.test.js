a442a4dbc97b795590f9f6082bf6da9b3fdfdb53
Your Name <rasa.alikonyte@gmail.com>
2024-05-13 18:21:17 +0300

    jes ir super test added

---
+++ index.test.js
@@ -0,0 +1,9 @@
+describe("test suite description", () => {
+  test("test description", () => {
+    expect(true).toBe(true);
+  });
+
+  test("test description 2", () => {
+    expect(true).toBe(false);
+  });
+});
